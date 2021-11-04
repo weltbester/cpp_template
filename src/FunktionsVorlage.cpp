9526d31aae15d0dcb3c128b9f6b06a1dca1a5653
Georg <48410861+weltbester@users.noreply.github.com>
2021-11-04 10:05:15 +0100

    Update FunktionsVorlage.cpp

--- src/FunktionsVorlage.cpp
+++ src/FunktionsVorlage.cpp
@@ -35,11 +35,4 @@
  * can arise due to wrong usage.
  */
 
-/* int foo(int x) {
-  !TODO: gpohl 2021-01.01
-  ... not longer than 2000 lines. */
-
-  return -1;
-} */
-
-/* End of filename.cpp */
+/* function() */
